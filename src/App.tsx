4b79fa30d5d6e5882a6290e1ae33d8757cf3d034
eden.kim <eden.kim@goorm.io>
2023-07-06 21:02:34 +0000

    route url edited

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,23 @@ import { LandingPage } from '@pages/Landing';
 import { NotFoundPage } from '@pages/NotFound';
 import { Layout } from '@layout/index';
 
-const staticServerUri = import.meta.env.VITE_REACT_APP_PATH || '';
+// const staticServerUri = import.meta.env.VITE_REACT_APP_PATH || '';
 
 function App() {
-  console.log('staticServerUri:', staticServerUri);
+  // console.log('staticServerUri:', staticServerUri);
   return (
     <BrowserRouter>
       <GlobalStyle />
       <GlobalFonts />
       <Layout>
         <Routes>
-          <Route path={staticServerUri + '/'} element={<LandingPage />} />
-          <Route path={staticServerUri + '/main'} element={<MainPage />} />
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/main' element={<MainPage />} />
           <Route
-            path={staticServerUri + '/main/:routeId/book'}
+            path='/main/:routeId/book'
             element={<GuestBookPage />}
           />
-          <Route path={staticServerUri + '*'} element={<NotFoundPage />} />
+          <Route path='*' element={<NotFoundPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
@@ -32,3 +32,12 @@ function App() {
 }
 
 export default App;
+
+
+
+          // <Route path={staticServerUri + '/main'} element={<MainPage />} />
+          // <Route
+          //   path={staticServerUri + '/main/:routeId/book'}
+          //   element={<GuestBookPage />}
+          // />
+          // <Route path={staticServerUri + '*'} element={<NotFoundPage />} />
