a0bfaaffa7d54f0fb81bdb32cec6d2dfac8d8fc5
eden.kim <eden.kim@goorm.io>
2023-07-06 20:59:10 +0000

    assets base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/k92ebc3e719eda/',
   plugins: [tsconfigPaths()],
   server: {
     port: 3000,
