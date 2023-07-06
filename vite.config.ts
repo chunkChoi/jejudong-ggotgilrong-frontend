ad8f3c8ced4e0944a57d62367c70c6fce8659c7e
chsi1995 <chsi1995@kakao.com>
2023-07-06 21:35:17 +0000

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,28 @@ import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      tsconfigPaths(),
-      ssr({
-          baseAssets: 'https://user-app.krampoline.com/kcb7af3d2b823a/'
-      })
-  ],
+  // plugins: [
+  //     tsconfigPaths(),
+  //     ssr({
+  //         baseAssets: 'https://user-app.krampoline.com/kcb7af3d2b823a/'
+  //     })
+  // ],
+plugins: [react()],
+	build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/kcb7af3d2b823a/[name]-[hash].js',
+        entryFileNames: 'assets/kcb7af3d2b823a/[name]-[hash].js',
+      },
+    },
+  },
   server: {
     port: 3000,
   },
