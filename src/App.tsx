d60504ffcdd593f0dfd83886cf55f99e295c09ce
chsi1995 <chsi1995@kakao.com>
2023-07-07 00:01:05 +0000

    adsf

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,10 @@ function App() {
       <GlobalFonts />
       <Layout>
         <Routes>
-          <Route path={staticServerUri + '/'} element={<LandingPage />} />
-          <Route path={staticServerUri + '/main'} element={<MainPage />} />
+          <Route path={'/'} element={<LandingPage />} />
+          <Route path={'/main'} element={<MainPage />} />
           <Route
-            path={staticServerUri + '/main/:routeId/book'}
+            path={'/main/:routeId/book'}
             element={<GuestBookPage />}
           />
           <Route path='*' element={<NotFoundPage />} />
