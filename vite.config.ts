39c314d6fc08d04a71a92353bc8e69df078cba3a
chsi1995 <chsi1995@kakao.com>
2023-07-07 01:26:06 +0000

    adsf

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { ssr } from 'vite-plugin-ssr/plugin'
+import { ssr } from 'vite-plugin-ssr/plugin';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-	build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `kcb7af3d2b823a/assets/${extType}/[name]-[hash][extname]`;
+    plugins: [
+        tsconfigPaths(),
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                // assetFileNames: (assetInfo) => {
+                //     let extType = assetInfo.name.split('.').at(1);
+                //     if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                //         extType = 'img';
+                //     }
+                //     return `assets/${extType}/[name]-[hash][extname]`;
+                // },
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'kcb7af3d2b823a/assets/[name]-[hash].js',
+            },
         },
-        chunkFileNames: '../kcb7af3d2b823a/assets/[name]-[hash].js',
-        entryFileNames: 'kcb7af3d2b823a/assets/[name]-[hash].js',
-      },
     },
-  },
-  server: {
-    port: 3000,
-  },
+    server: {
+        port: 3000,
+    },
 });
